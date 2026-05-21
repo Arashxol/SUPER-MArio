8a4c1e74fff11bb21234a56c1a2ba98b59e04861
Arashxol <joelcitochoquecito76@gmail.com>
2026-05-21 16:20:04 -0400

    1-2 gameover

---
+++ Assets/Scripts/MenuPrincipal.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPrincipal : MonoBehaviour
+{
+    public void Jugar()
+    {
+        SceneManager.LoadScene("1-1");
+    }
+
+    public void Salir()
+    {
+        Application.Quit();
+
+        Debug.Log("Salir del juego");
+    }
+}
